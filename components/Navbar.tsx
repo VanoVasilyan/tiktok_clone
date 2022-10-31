12be68095d6be896765a3b2c11b7c17cd51f4d74
Vano <vanovasilyan@gmail.com>
2022-10-31 12:53:22 +0400

    create utils folder and build Navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { GoogleLogin, googleLogout } from '@react-oauth/google';
+import { AiOutlineLogout } from 'react-icons/ai';
+import { BiSearch } from 'react-icons/bi';
+import { IoMdAdd } from 'react-icons/io';
+import logo from '../utils/tiktok-logo.png';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
+      <Link href='/'>
+        <div className='w-[100px] md:w-[130px] ms:h-[30px] h-[38px]'>
+          <Image
+            className='cursor-pointer'
+            src={logo}
+            alt='tiktok'
+            layout='responsive'
+          />
+        </div>
+      </Link>
+    </div>
   )
 }
 
