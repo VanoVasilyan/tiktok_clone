509e6a8836aaa22b9506c03a8a57557163ee04db
Vano <vanovasilyan@gmail.com>
2022-11-02 14:32:34 +0400

    build Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import { footerList1, footerList2, footerList3 } from '../utils/constants';
+
+type ListProps = {
+  list: string[];
+  mt: boolean
+}
+
+const List = ({ list, mt }: ListProps) => (
+  <div className={`flex flex-wrap gap-2 ${mt ? 'mt-5' : ''}`}>
+    {list.map((item) => (
+      <p key={item} className='text-gray-400 text-sm hover:underline cursor-pointer'>
+        {item}
+      </p>
+    ))}
+  </div>
+)
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='mt-6 hidden xl:block'>
+      <List list={footerList1} mt={false} />
+      <List list={footerList2} mt />
+      <List list={footerList3} mt />
+      <p className='text-gray-400 text-sm mt-5'>
+        {new Date().getFullYear()} TikTok Clone
+      </p>
+    </div>
   )
 }
 
