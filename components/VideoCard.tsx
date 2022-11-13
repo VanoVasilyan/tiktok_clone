d3b3c7fc95a37b6410c1a18b4c72ecaf18fa3dfd
Vano <vanovasilyan@gmail.com>
2022-11-13 15:00:57 +0400

    fetch profile info from backend

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -36,30 +36,26 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
     return (
         <div className='flex flex-col border-b-2 border-gray-200 pb-6'>
             <div>
-                <div className='flex gap-3 p-5 cursor-pointer font-semibold rounded'>
-                    <div className='md:w-16 md:h-16 w-10 h-10'>
-                        <Link href='/'>
-                            <>
-                                <Image
-                                    width={62}
-                                    height={62}
-                                    className='rounded-full'
-                                    src={post.postedBy.image}
-                                    alt='profile photo'
-                                    layout='responsive'
-                                />
-                            </>
-                        </Link>
-                    </div>
-                    <div>
-                        <Link href='/'>
+                <Link href={`/profile/${post.postedBy._id}`}>
+                    <div className='flex gap-3 p-5 cursor-pointer font-semibold rounded'>
+                        <div className='md:w-16 md:h-16 w-10 h-10'>
+                            <Image
+                                width={62}
+                                height={62}
+                                className='rounded-full'
+                                src={post.postedBy.image}
+                                alt='profile photo'
+                                layout='responsive'
+                            />
+                        </div>
+                        <div>
                             <div className='flex items-center gap-2'>
                                 <p className='flex gap-2 items-center md:text-md font-bold text-primary'>{post.postedBy.userName} <GoVerified className='text-blue-400 text-md' /></p>
                                 <p className='capitalize font-medium text-xs text-gray-500 hidden md:block'>{post.postedBy.userName}</p>
                             </div>
-                        </Link>
+                        </div>
                     </div>
-                </div>
+                </Link>
             </div>
             <div className='lg:ml-20 flex gap-4 relative'>
                 <div
--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Image from 'next/image';
+import { GoVerified } from 'react-icons/go';
+import VideoCard from '../../components/VideoCard';
+import NoResults from '../../components/NoResults';
+import { IUser, Video } from '../../types';
 
-const Profile = () => {
+interface IProps {
+    data: {
+        user: IUser;
+        userVideos: Video[];
+        userLikedVideos: Video[]
+    }
+}
+
+const Profile = ({ data }: IProps) => {
     return <div>
         Profile
     </div>
+}
+
+export const getServerSideProps = async ({ params: { id } }: { params: { id: string } }) => {
+    const res = await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/api/profile/${id}`);
+
+    return {
+        props: {
+            data: res.data
+        }
+    }
 }
 
 export default Profile
